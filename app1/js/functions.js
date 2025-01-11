1dd821eea055ba9ca108eef74d98a212c1e232f1
BooleanDew <booleandew@gmail.com>
2025-01-11 18:24:57 -0300

    UPDATE: se agregan nuevas funcionalidades.

--- app1/js/functions.js
+++ app1/js/functions.js
@@ -1,18 +1,55 @@
+// componentes DOM.
 let nombre = document.querySelector("#nombre");
 let descripcion = document.querySelector("#descripcion");
 let btnCrear = document.querySelector("#btnCrear");
 let tasks = document.querySelector("#tasks");
+let toask = document.querySelector("#toask");
 
-btnCrear.addEventListener("click", () => {
+let fecha = new Date()
 
-    localStorage.setItem(nombre.value, descripcion.value);
-    location.reload();
+
+btnCrear.addEventListener("click", (event) => {
+
+    if (event.target.innerHTML === "Editar") {
+        localStorage.setItem(nombre.value, JSON.stringify({"descripcion":descripcion.value,"estado":1}));
+        location.reload();
+    } else {
+        if (localStorage.getItem(nombre.value)) {
+            toask.style.setProperty("display", "block");
+            toask.style.setProperty("opacity", "8");
+            toask.innerHTML = `<i class="fa-solid fa-circle-info"></i> El nombre de la tarea ya existe.`;
+            setTimeout(() => {
+                toask.style.setProperty("opacity", "0.1");
+                toask.style.setProperty("--webkit-transition", "opacity 2s");
+                toask.style.setProperty("display", "none");
+            }, 2500);
+        } else {
+            if ((nombre.value != "") && (descripcion.value != "")) {
+                localStorage.setItem(nombre.value, JSON.stringify({"descripcion":descripcion.value,"estado":1}));
+                location.reload();
+            } else {
+                toask.style.setProperty("display", "block");
+                toask.style.setProperty("opacity", "8");
+                toask.innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i> No se permiten campos vacios.`;
+                setTimeout(() => {
+                    toask.style.setProperty("opacity", "0.1");
+                    toask.style.setProperty("--webkit-transition", "opacity 2s");
+                    toask.style.setProperty("display", "none");
+                }, 2500);
+            }
+        }
+
+    }
+
 });
 
 
 for (let i = 0; i < localStorage.length; i++) {
-    console.log(localStorage.key(i))
-    tasks.innerHTML += `<div class='task'><button onClick="delTask('${localStorage.key(i)}')" class="btnDelete">X</button><div class='title' id='titleTask'><i class="fa-regular fa-star"></i>${localStorage.key(i)}</div><p>${localStorage.getItem(localStorage.key(i))}</p></div>`;
+   
+    tasks.innerHTML += `<div class='task' onClick="editTask('${localStorage.key(i)}','${JSON.parse(localStorage.getItem(localStorage.key(i))).descripcion}')"><button onClick="delTask('${localStorage.key(i)}')" class="btnDelete">X</button><div class='title' id='titleTask'><i class="fa-regular fa-star"></i>${localStorage.key(i)}</div><p>${JSON.parse(localStorage.getItem(localStorage.key(i))).descripcion}</p><div class="fechaTask">${fecha.getDate()}/${fecha.getMonth()}/${fecha.getFullYear()}</div></div>`;
+    if(JSON.parse(localStorage.getItem(localStorage.key(i))).estado == 1){
+        document.querySelector(".task").style.setProperty("border-left","4px solid green")
+    }
 }
 
 function delTask(id) {
@@ -20,4 +57,9 @@ function delTask(id) {
     location.reload();
 }
 
+function editTask(nombreTask, descripcionTask) {
+    btnCrear.innerHTML = "Editar";
+    document.querySelector("#nombre").value = nombreTask;
+    document.querySelector("#descripcion").value = descripcionTask;
+}
 
